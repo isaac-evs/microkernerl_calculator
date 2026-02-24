acdd8508186634017d08f0eba36b485ef1c6fef8
isaac-evs <isaac.vazquez@iteso.mx>
2026-02-24 10:32:22 -0600

    ignored venv

---
+++ operations/sqrt.py
@@ -0,0 +1,16 @@
+import math
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from interfaces import OperationPlugin
+
+
+class Multiplicacion(OperationPlugin):
+    @property
+    def name(self):
+        return "√"
+
+    def execute(self, a, b):
+        return math.sqrt(a + b)
